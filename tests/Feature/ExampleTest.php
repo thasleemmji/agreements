eba0b7f8c9bd9d300d7372da431e9b85f8ea511d
Thasleem <thasleem@salesdock.nl>
2021-05-25 17:53:40 +0530

    fix: changes

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,11 +7,6 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_example()
     {
         $response = $this->get('/');
